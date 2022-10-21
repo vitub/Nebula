76cc15285bdd21c26f56a66bcc62d4edf550e65a
vitub <victorpsnemail@gmail.com>
2022-10-20 21:28:08 -0300

    🚀 first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var intervald = window.setInterval(() => {load()
+
+        
+
+},1000);
+
+
+function load() {
+    var msg = document.getElementById('hour')
+    var img = document.getElementById('img')
+    var data =new Date()
+    var hour = (data.getHours() < 10? '0' : '') + data.getHours();
+    //var hour = data.getHours()
+    var min = (data.getMinutes() < 10? '0' : '') + data.getMinutes();
+    //var min = data.getMinutes()
+    msg.innerHTML = `${hour}:${min}`
+    document.body.background = 'https://source.unsplash.com/random/1920x1080/?Night'  
+    
+    
+}
